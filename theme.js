97774b7da6361af2979d441c5de5142ebbcec1b6
nothingisboring <neilsfitzgerald@gmail.com>
2025-04-26 16:52:52 +0200

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,21 @@
+// themes.js
+const THEME_LIBRARY = {
+  "default": {
+    // Default CSS variables
+  },
+  "mondrian": {
+    "--clr-bg": "#FFFFFF",
+    "--clr-text-default": "#000000",
+    "--clr-primary": "#FF0000",
+    "--clr-box-bg-back": "#FFFF00",
+    "--clr-box-bg-final": "#0000FF",
+    // Additional theme variables
+  },
+  "film-noir": {
+    "--clr-bg": "#121212",
+    "--clr-text-default": "#FFFFFF",
+    "--clr-primary": "#8E8E8E",
+    // Film noir theme
+  },
+  // Additional themes
+}
